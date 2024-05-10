8a6583c266fa5407e8066f3182746b4c93de013e
Rafie <rafiekhana@gmail.com>
2024-05-10 10:37:13 +0700

    mengedit file

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,11 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array(
-  'form_validation',
-  'session',
-  'database'
-);
+$autoload['libraries'] = array();
 
 /*
 | -------------------------------------------------------------------
@@ -93,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url');
+$autoload['helper'] = array();
 
 /*
 | -------------------------------------------------------------------
@@ -136,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('ModelUser', 'ModelBuku');
+$autoload['model'] = array();
